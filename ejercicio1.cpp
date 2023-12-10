4c4c043033a5f52e5e24188f621659274a4e947e
AndreidForGit <elequipobizarro@gmail.com>
2023-12-09 23:40:22 -0400

    first

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -13,13 +13,15 @@ int main(){
   cin >> third;
 
   if (first <= second && second <= third) {
-    cout << "La secuencia de números " << first << ", " << second << ", " << third <<  " está en orden numérico" << endl;
+    cout << "La secuencia de números " << first << ", " << second << ", " << third <<  " está en orden numérico ascendente." << endl;
+  } else if (first >= second && second >= third) {
+    cout << "La secuencia de números " << first << ", " << second << ", " << third <<  " está en orden numérico descendente." << endl;
   } else {
-    cout << "La secuencia de números " << first << ", " << second << ", " << third <<  " no está en orden numérico"<<endl;
+    cout << "La secuencia de números " << first << ", " << second << ", " << third <<  " no está en orden numérico."<<endl;
     int arr[] = {first, second, third};
     int n = sizeof(arr) / sizeof(arr[0]);
     sort(arr, arr + n);
-    cout << "La secuencia de números ordenada seria: ";
+    cout << "La secuencia de números en orden ascendente seria: ";
     for (int i = 0; i < n; i++){
       cout << arr[i] << " ";
     };
